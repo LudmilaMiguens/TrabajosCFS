f5ba050f9ed5aa1eccebaa866a690238cfe3a3c8
LudmilaMiguens <miguensludmila2@gamil.com>
2023-03-15 21:53:09 -0300

    Agrege archivos

---
+++ fotosEstrellas.ts
@@ -0,0 +1,8 @@
+console.log("colocar la camara en un tripode");
+console.log("Utilizar el modo Manual");
+console.log("Poner el foco en Manual y hacer foto");
+console.log("Abrir lo que mas se pueda el obturador");
+console.log("Comenzar con un iso base de 3200 y una velocidad de 1/15 o 1/30");
+console.log("Gatillar y esperar el resultado"); 
+
+console.log("Fotos a las estrellas:" + " Paso 1: colocar la camara en un tripode," + " Paso 2: Utilizar el modo Manual," + " Paso 3: Poner el foco en Manual y hacer foto," + " Paso 4: Abrir lo que mas se pueda el obturador," + " Paso 5: Comenzar con un iso base de 3200 y una velocidad de 1/15 o 1/30," + " Paso 6: Gatillar y esperar el resultado");
