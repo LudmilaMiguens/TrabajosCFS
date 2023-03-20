a7bf267660877502bcbec56c7d1a5802b8c1359c
LudmilaMiguens <miguensludmila2@gamil.com>
2023-03-20 19:57:30 -0300

    cambios

---
+++ PROGRAMACION/MODULO1/SLIDE2/index.ts
@@ -0,0 +1,9 @@
+// delcaramos variables con los tipos primitivos
+let num: number = 12;
+console.log( num );
+num = 23
+
+let nom = "segismundo Roberto";
+console.log("el tipo de la variable nom es", typeof nom);
+
+const pi: number = 3.14
---
+++ PROGRAMACION/MODULO1/SLIDE2/tareaGranjera.ts
@@ -0,0 +1,30 @@
+console.log("avanzar uno");
+console.log("quitar uno");
+console.log("avanzar uno");
+console.log("quitar uno");
+console.log("avanzar uno");
+console.log("quitar uno");
+console.log("avanzar uno");
+console.log("quitar uno");
+console.log("girar izquierda 90 grados");
+console.log("avanzar uno");
+console.log("quitar uno");
+console.log("avanzar uno");
+console.log("quitar uno");
+console.log("avanzar uno");
+console.log("quitar uno");
+console.log("avanzar uno");
+console.log("quitar uno");
+
+//programa 2
+
+console.log("avanzar uno");
+console.log("llenar uno");
+console.log("avanzar uno");
+console.log("avanzar uno");
+console.log("avanzar uno");
+console.log("llenar uno");
+console.log("avanzar uno");
+console.log("quitar uno");
+console.log("avanzar uno");
+console.log("quitar uno");
