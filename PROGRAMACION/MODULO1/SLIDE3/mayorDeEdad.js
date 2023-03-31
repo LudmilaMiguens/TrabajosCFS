27c1aff910dc87038eb3e893b0ab66126a4ddc40
LudmilaMiguens <miguensludmila2@gamil.com>
2023-03-31 15:51:13 -0300

    trabajo terminado

---
+++ PROGRAMACION/MODULO1/SLIDE3/mayorDeEdad.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+var rls = require("readline-sync");
+// ejercicio validar altura
+/*let alturaUsuario : number = rls.questionInt("Indique su altura");
+
+if (alturaUsuario > 130) {
+    console.log("Ustdes puede subir");
+
+}  else {
+    console.log("Ustdes no puede subir");
+}*/
+// ejercicio longi
+var usuario = rls.string("igrese su usuario");
+var contraseña = rls.string("igrese su usuario");
+console.log("ingresu usuario:", usuario);
+console.log("ingrese contraseña", contraseña);
---
+++ PROGRAMACION/MODULO1/SLIDE3/mayorDeEdad.ts
@@ -0,0 +1,21 @@
+import * as rls from "readline-sync";
+
+// ejercicio validar altura
+
+/*let alturaUsuario : number = rls.questionInt("Indique su altura");
+
+if (alturaUsuario > 130) {
+    console.log("Ustdes puede subir");
+
+}  else {
+    console.log("Ustdes no puede subir");
+}*/
+
+// ejercicio longi
+
+
+
+let Juan : boolean = true
+let claveJuan: boolean = true
+
+let usuario : string = rls.keyInYNStrict("ingrese usuario");
