1680fb339811ef4df359ea4dc3328914e0b1cf79
LudmilaMiguens <miguensludmila2@gamil.com>
2023-04-24 11:32:52 -0300

    clase 7

---
+++ PROGRAMACION/MODULO1/SLIDE7/funcion.ts
@@ -0,0 +1,17 @@
+// DECLARACION DE FUNCIONES
+// Tres formas de declarar funciones
+
+function otraFuncion(){
+    console.log("Hola soy la primera funcion");
+}
+otraFuncion(); //aca estamos llamando la funcion
+
+let miFuncion = function(){
+    console.log("Hola soy la segunda funcion");
+}
+miFuncion(); //aca estamos llamando la funcion
+
+let arrowFuncion = () => {
+    console.log("Hola soy la tercera funcion");
+}
+arrowFuncion(); //aca estamos llamando la funcion
